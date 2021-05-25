3c396142e6edb94be740db6f6f5c1a077cec6917
yvonnevictoria <28718897+yvonnevictoria@users.noreply.github.com>
2021-05-25 14:26:10 +1000

    Create portfolioService to manage user portfolio

---
+++ server/services/portfolioService.js
@@ -0,0 +1,76 @@
+'use strict';
+
+
+/**
+ * @module
+ */
+
+ const portfolio = {
+     GOOG: 1
+ };
+
+/**
+ * @classdesc Retrieves and updates information about user's stock portfolio.
+ * @hideconstructor
+ */
+class PortfolioService {
+
+    /**
+     * Gets the user's portfolio.
+     *
+     * @returns {Object} The user's portfolio.
+     */
+    static async getPortfolio() {
+        return portfolio;
+    }
+
+    /**
+     * Gets the user's portfolio.
+     *
+     * @param {Object} obj - The arguments object.
+     * @param {String} obj.stockSymbol - The stockSymbol of the stocks purchased.
+     * @returns {Number} The stock amount for specified symbol.
+     */
+    static async getCurrentStockAmount({ stockSymbol }) {
+        return portfolio[stockSymbol] || 0;
+    }
+
+    /**
+     * Adds purchased stock to the user's portfolio.
+     *
+     * @param {Object} obj - The arguments object.
+     * @param {String} obj.amount - The amount of stocks purchased.
+     * @param {String} obj.stockSymbol - The stockSymbol of the stocks purchased.
+     * @returns {String} The updated portfolio.
+     */
+    static async addToPortfolio({ stockSymbol, amount }) {
+        const existingStocks = portfolio[stockSymbol] ? portfolio[stockSymbol].amount : 0;
+
+        portfolio[stockSymbol] = amount + existingStocks;
+        return portfolio;
+    }
+
+    /**
+     * Remove sold stock from user's porfolio.
+     *
+     * @param {Object} obj - The arguments object.
+     * @param {String} obj.removedStocks - The amount of stocks sold.
+     * @param {String} obj.stockSymbol - The stockSymbol of the stocks sold.
+     * @returns {String} The updated portfolio.
+     */
+    static async removeFromPortfolio({ stockSymbol, removedStocks }) {
+        const existingStocks = portfolio[stockSymbol];
+
+        if (existingStocks < removedStocks) {
+            throw new Error('INSUFFICIENT_BALANCE');
+        }
+
+        removedStocks === existingStocks
+        ? delete portfolio[stockSymbol]
+        : portfolio[stockSymbol] = existingStocks - removedStocks
+
+        return portfolio;
+    }
+}
+
+module.exports = { PortfolioService };
---
+++ server/services/portfolioService.spec.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const test = require('ava');
+const sinon = require('sinon');
+const { PortfolioService } = require('./PortfolioService');
+const sandbox = sinon.createSandbox();
+
+test.afterEach.always(async () => {
+    sandbox.restore();
+});
+
+test.serial(`getPortfolio | should return user's current portfolio`, async t => {
+    t.deepEqual(await PortfolioService.getPortfolio(), {
+        GOOG: 1
+    });
+});
+
+test.serial('getCurrentStockAmount | should return currently held amount of specified stock', async t => {
+    const heldAmount = await PortfolioService.getCurrentStockAmount({ stockSymbol: 'GOOG' });
+
+    t.is(heldAmount, 1);
+});
+
+test.serial('addToPortfolio | should add specified stock and amount to portfolio', async t => {
+    const heldAmountOriginal = await PortfolioService.getCurrentStockAmount({ stockSymbol: 'IBM' });
+    t.is(heldAmountOriginal, 0);
+
+    t.deepEqual(await PortfolioService.addToPortfolio({ stockSymbol: 'IBM', amount: 4 }), {
+        GOOG: 1,
+        IBM: 4
+    });
+
+    const heldAmountUpdated = await PortfolioService.getCurrentStockAmount({ stockSymbol: 'IBM' });
+    t.is(heldAmountUpdated, 4);
+});
+
+test.serial('removeFromPortfolio | should remove specified amount of stock from portfolio', async t => {
+    const heldAmountOriginal = await PortfolioService.getCurrentStockAmount({ stockSymbol: 'IBM' });
+    t.is(heldAmountOriginal, 4);
+
+    t.deepEqual(await PortfolioService.removeFromPortfolio({ stockSymbol: 'IBM', removedStocks: 2 }), {
+        GOOG: 1,
+        IBM: 2
+    });
+
+    const heldAmountUpdated = await PortfolioService.getCurrentStockAmount({ stockSymbol: 'IBM' });
+    t.is(heldAmountUpdated, 2);
+});
+
+test.serial('removeFromPortfolio | should remove specified stock from portfolio if entire amount is removed', async t => {
+    const heldAmountOriginal = await PortfolioService.getCurrentStockAmount({ stockSymbol: 'IBM' });
+    t.is(heldAmountOriginal, 2);
+
+    t.deepEqual(await PortfolioService.removeFromPortfolio({ stockSymbol: 'IBM', removedStocks: 2 }), {
+        GOOG: 1
+    });
+
+    const heldAmountUpdated = await PortfolioService.getCurrentStockAmount({ stockSymbol: 'IBM' });
+    t.is(heldAmountUpdated, 0);
+});
+
+test.serial('removeFromPortfolio | should throw INSUFFICIENT_BALANCE error if stocks sold is higher than existing stocks', async t => {
+    const heldAmountOriginal = await PortfolioService.getCurrentStockAmount({ stockSymbol: 'GOOG'});
+    t.is(heldAmountOriginal, 1);
+
+    const err = await t.throwsAsync(PortfolioService.removeFromPortfolio({
+        stockSymbol: 'GOOG',
+        removedStocks: 2
+    }));
+
+    t.is(err.message, 'INSUFFICIENT_BALANCE');
+
+});
